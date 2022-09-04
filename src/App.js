ad70493f1d054836a691cc5348db66dff2a5b4ab
Gabriela Florêncio <perflogabi@gmail.com>
2022-09-03 23:00:53 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Banner from "./componentes/Banner/Banner";
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        Banner / >
+        <
+        /div>
+    );
+}
+
+export default App;
