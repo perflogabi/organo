fce9e71e2d84300e078b3791e560241523faebf8
Gabriela Florêncio <perflogabi@gmail.com>
2022-09-03 23:01:40 -0300

    primeiro commit

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,15 +1,14 @@
 import "./Banner.css";
 
 function Banner() {
-    return ( <
-        header className = "banner" >
-        <
-        img src = "/imagens/banner.png"
-        alt = "O banner principal da página do Organo" /
-        >
-        <
-        /header>
-    );
+  return (
+    <header className="banner">
+      <img
+        src="/imagens/banner.png"
+        alt="O banner principal da página do Organo"
+      />
+    </header>
+  );
 }
 
 export default Banner;
