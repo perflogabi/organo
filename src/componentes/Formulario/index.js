138906405227da6499e4cc1bea9c21830ca9590e
Gabriela Florêncio <perflogabi@gmail.com>
2022-09-06 23:29:36 -0300

    quarto commit

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -5,16 +5,7 @@ import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 const Formulario = (props) => {
-  const times = [
-    "Programação",
-    "Front-end",
-    "Data-Science",
-    "Devops",
-    "UX e Design",
-    "Mobile",
-    "Inovação e Gestão",
-  ];
-
+ 
   const [nome, setNome] = useState('')
   const [cargo, setCargo] = useState('')
   const [imagem, setImagem] = useState('')
@@ -58,7 +49,7 @@ const Formulario = (props) => {
         <ListaSuspensa 
         obrigatorio={true} 
         label="Time" 
-        itens={times} 
+        itens={props.times} 
         valor={time}
         aoAlterado={valor => setTime(valor)}
         />
